e49e32bbf41b35e314a7595bf983a4414152a96d
Bhavesh S Singh <bhaveshsinghbiz@gmail.com>
2021-01-27 17:51:01 +0530

    inital commit

---
+++ src/components/Footer.js
@@ -0,0 +1,16 @@
+import { Link } from "react-router-dom";
+
+const Footer = () => {
+  return (
+    <footer>
+      <p>
+        {" "}
+        <a href="/About">About</a>
+      </p>
+      Made with a lot of ☕ by
+      <a href="https://twitter.com/Bhaveshh_Singh"> Bhavesh Singh</a>{" "}
+    </footer>
+  );
+};
+
+export default Footer;
